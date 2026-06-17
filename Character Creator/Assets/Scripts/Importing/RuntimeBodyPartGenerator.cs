3878585ac6d507ff77127132340ff8f107292e9a
Joeri <84157343+Diedeliedee@users.noreply.github.com>
2026-06-17 14:04:43 +0200

    Fix texture issue

--- Character Creator/Assets/Scripts/Importing/RuntimeBodyPartGenerator.cs
+++ Character Creator/Assets/Scripts/Importing/RuntimeBodyPartGenerator.cs
@@ -1,11 +1,14 @@
 using BodyPartSwap;
 using GLTFast;
+using GLTFast.Schema;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class RuntimeBodyPartGenerator : MonoBehaviour
 {
@@ -37,31 +40,16 @@ public class RuntimeBodyPartGenerator : MonoBehaviour
         //  Iterate through all the meshes.
         foreach (var mesh in instantiatedRenderers)
         {
-            //  Scan if the mesh name corresponds to any of the established part types.
-            foreach (Options partType in Enum.GetValues(typeof(Options)))
+            //  Check if the mesh contains the attributes of a bodypart.
+            if (!MatchesPartAttributes(mesh, out Options _type))
             {
-                //  If the name doesn't correspond. Continue.
-                if (!mesh.sharedMesh.name.Contains(partType.ToString(), StringComparison.OrdinalIgnoreCase))
-                {
-                    Debug.Log($"Imported model part {mesh.sharedMesh.name} is not a bodypart type of: {partType.ToString()}. Continuing probe.", this);
-                    continue;
-                }
-
-                //  If the name does correspond. Create a PartInfo instance.
-                var partInstance = ScriptableObject.CreateInstance<PartInfo>();
-
-                //  Set it up, and add it to the list.
-                partInstance.name = mesh.gameObject.name;
-                partInstance.Setup(partType, Instantiate(mesh.sharedMesh), Instantiate(mesh.sharedMaterial));
-                partInstances.Add(partInstance);
-
-                //  Debug!!
-                Debug.Log($"Succesfully detected model part {mesh.gameObject.name} is a {partType.ToString()}.", this);
+                Debug.LogWarning($"Did not find suitable bodypart type for model part {mesh.gameObject.name}.", this);
                 continue;
             }
 
-            //  If the loop is complete, the model part did not match any of the part names.
-            Debug.LogWarning($"Did not find suitable bodypart type for model part {mesh.gameObject.name}.", this);
+            //  Add the generated partinfo to the list.
+            Debug.Log($"Succesfully detected model part {mesh.gameObject.name} is a {_type.ToString()}.", this);
+            partInstances.Add(CreatePartInfo(_type, mesh));
         }
 
         //  If no suitable bodypart were found, log it.
@@ -69,12 +57,59 @@ public class RuntimeBodyPartGenerator : MonoBehaviour
         {
             Debug.LogError($"Found absolutely NO suitable bodyparts in model of {_path}.. :(", this);
 
-            importInstance.Dispose();
+            Cleanup();
             return null;
         }
 
         //  If there are, pull them back up and stuff.
-        importInstance.Dispose();
+        Cleanup();
         return partInstances;
+    }
+
+    private PartInfo CreatePartInfo(Options _type, SkinnedMeshRenderer _mesh)
+    {
+        var partInstance = ScriptableObject.CreateInstance<PartInfo>();
+
+        //  Set it up.
+        partInstance.name = _mesh.gameObject.name;
+        partInstance.Setup(_type, _mesh.sharedMesh, _mesh.sharedMaterial);
+
+        //  Correctly duplicating materials
+        //partInstance.material.CopyPropertiesFromMaterial(_mesh.sharedMaterial);
+
+        //  Return!
+        return partInstance;
+    }
+
+    private bool MatchesPartAttributes(SkinnedMeshRenderer _mesh, out Options _type)
+    {
+        _type = Options.Head;
+
+        //  Scan if the mesh name corresponds to any of the established part types.
+        foreach (Options partType in Enum.GetValues(typeof(Options)))
+        {
+            //  If the name doesn't correspond. Continue.
+            if (!_mesh.sharedMesh.name.Contains(partType.ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log($"Imported model part {_mesh.sharedMesh.name} is not a bodypart type of: {partType.ToString()}. Continuing probe.", this);
+                continue;
+            }
+
+            //  If it does, epic!
+            _type = partType;
+            return true;
+        }
+
+        //  If the loop is complete, the model part did not match any of the part names.
+        return false;
+    }
+
+    private void Cleanup()
+    {
+        /// Disposing the import instance means destroying the memory that holds the mesh, material, and texture data.
+        /// While I did try to preserve that by copying the data, I might as well destroy just the gameobject instead.
+        //importInstance.Dispose();
+
+        Destroy(transform.GetChild(0).gameObject);
     }
 }
