2307a8a131011cb3fc58b01fdd6f6317787f9ee5
Joeri <84157343+Diedeliedee@users.noreply.github.com>
2026-06-21 17:31:09 +0200

    Implement independent body part scaling

--- Character Creator/Assets/Sections/Character Creation/Body Part Swapping/ActiveBodyPart.cs
+++ Character Creator/Assets/Sections/Character Creation/Body Part Swapping/ActiveBodyPart.cs
@@ -7,14 +7,18 @@ namespace BodyPartSwap
         [Header("Reference:")]
         [SerializeField] private OptionQueue m_queue;
         [SerializeField] private SkinnedMeshRenderer m_meshRenderer;
+        [Space]
+        [SerializeField] private ActiveBodyPart m_ChildBodyPart = null;
 
         //  Run-time:
         private int m_index                 = 0;
         private PartInfo m_selectedPartInfo = null;
+        private Vector3 m_scale             = Vector3.one;
+        private Vector3 m_modifier          = Vector3.one;
 
         public Options type     => m_queue.type;
         public int index        => m_index;
-        public Vector3 scale    => transform.localScale;
+        public Vector3 scale    => m_scale;
 
         public string typeName      => type.ToString();
         public string selectedName  => m_selectedPartInfo ? m_selectedPartInfo.name : "Null :(";
@@ -54,22 +58,37 @@ namespace BodyPartSwap
 
         public void ProcessScale(int _axis, float _scale)
         {
-            var scale = transform.localScale;
-
             switch (_axis)
             {
-                case 0: scale.x = _scale; break;
-                case 1: scale.y = _scale;  break;
-                case 2: scale.z = _scale; break;
+                case 0: m_scale.x = _scale; break;
+                case 1: m_scale.y = _scale; break;
+                case 2: m_scale.z = _scale; break;
             }
 
-            transform.localScale = scale;
+            m_ChildBodyPart?.ProcessScaleModifier(m_scale);
+            transform.localScale = CalculatedScale();
         }
 
         public void ProcessScale(Vector3 _scale)
         {
-            transform.localScale = _scale;
+            m_scale = _scale;
+            m_ChildBodyPart?.ProcessScaleModifier(_scale);
+
+            transform.localScale = CalculatedScale();
         }
+
+        public void ProcessScaleModifier(Vector3 _parentScale)
+        {
+            var modifier = Vector3.one;
+
+            if (_parentScale.x > 0) modifier.x /= _parentScale.x;
+            if (_parentScale.y > 0) modifier.y /= _parentScale.y;
+            if (_parentScale.z > 0) modifier.z /= _parentScale.z;
+
+            m_modifier              = modifier;
+            transform.localScale    = CalculatedScale();
+        }
+
 
         public bool ContainsBodyPart(PartInfo _bodyPart)
         {
@@ -79,6 +98,17 @@ namespace BodyPartSwap
         public void AddToQueue(PartInfo _bodyPart)
         {
             m_queue.AddToQueue(_bodyPart);
+        }
+
+        private Vector3 CalculatedScale()
+        {
+            var scale = m_scale;
+
+            scale.x *= m_modifier.x;
+            scale.y *= m_modifier.y;
+            scale.z *= m_modifier.z;
+
+            return scale;
         }
     }
 }
